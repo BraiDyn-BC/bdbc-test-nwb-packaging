943dec8564c990ac560af5ba5675ec6e611772b9
Keisuke Sehara <keisuke.sehara@gmail.com>
2024-10-29 09:48:00 +0900

    impl: test rawdata

--- tests/test_single.py
+++ tests/test_single.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from datetime import datetime
 import os
 import json
+import shutil
 
 import numpy as np
 import pandas as pd
@@ -13,16 +15,38 @@ load_dotenv()
 
 DEBUG = bool(os.getenv('DEBUG'))
 
+TEMP_NWBROOT = Path(os.getenv('TEMP_NWBROOT_DIR'))
+
 TASK_SESSION_INFO = json.loads(os.getenv('TASK_SESSION_INFO'))
-TASK_SESSION_AVAILABILITY = json.loads(os.getenv('TASK_SESSION_AVAILABILITY'))
 TASK_RAWDATA_PATH = Path(os.getenv('TASK_RAWDATA_PATH'))
 TASK_BODY_VIDEO = Path(os.getenv('TASK_BODY_VIDEO_PATH'))
 TASK_FACE_VIDEO = Path(os.getenv('TASK_FACE_VIDEO_PATH'))
 TASK_EYE_VIDEO = Path(os.getenv('TASK_EYE_VIDEO_PATH'))
 
+TASK_SESSION_INFO['date'] = datetime.strptime(
+    TASK_SESSION_INFO['date'], '%y%m%d'
+)
+
+
+def setup_session_availability() -> sessx.session.Availability:
+    avail = dict()
+    for name, env in (
+        ('rawdata', 'TASK_RAWDATA_PATH'),
+        ('bodyvideo', 'TASK_BODY_VIDEO_PATH'),
+        ('facevideo', 'TASK_FACE_VIDEO_PATH'),
+        ('eyevideo', 'TASK_EYE_VIDEO_PATH'),
+    ):
+        val = os.getenv(env)
+        avail[name] = ((val is not None) and len(val.strip()) > 0)
+    return sessx.session.Availability(**avail)
+
 
 def load_paths() -> npack.paths.PathSettings:
-    avail = sessx.session.Availability(**TASK_SESSION_AVAILABILITY)
+    session = sessx.Session(
+        availability=setup_session_availability(),
+        **TASK_SESSION_INFO
+    )
+
     videos = npack.paths.VideoDataFiles(
         body=npack.paths.VideoDataFile(TASK_BODY_VIDEO, 0, 0, 0),  # FIXME: no metadata for the time being  # noqa: E501
         face=npack.paths.VideoDataFile(TASK_FACE_VIDEO, 0, 0, 0),  # FIXME: no metadata for the time being  # noqa: E501
@@ -35,11 +59,15 @@ def load_paths() -> npack.paths.PathSettings:
         mesoscaler=None,
         pupilfitting=None
     )  # FIXME
+    dest = npack.paths.setup_destination_paths(
+        session=session,
+        nwbroot=TEMP_NWBROOT,
+    )
     return npack.paths.PathSettings(
-        session=sessx.Session(availability=avail, **TASK_SESSION_INFO),
+        session=session,
         source=source,
         dlc_configs=None,
-        destination=None,
+        destination=dest,
     )
 
 
@@ -50,7 +78,7 @@ def test_paths():
     assert paths.source.rawdata.exists()
 
 
-def test_task_rawdata_handling():
+def test_base():
     paths = load_paths()
 
     # metadata
@@ -66,8 +94,6 @@ def test_task_rawdata_handling():
     )
     assert triggers is not None
     assert timebases is not None
-    # assert triggers.B.size == timebases.B.size
-    # assert triggers.V.size == timebases.V.size
     assert triggers.B.size <= timebases.B.size  # TODO: check if it is OK
     assert triggers.V.size <= timebases.V.size  # TODO: check if it is OK
     assert triggers.videos.size == timebases.videos.size
@@ -75,16 +101,79 @@ def test_task_rawdata_handling():
     assert_pulse_freq(timebases.V, 29, 31)
     assert_pulse_freq(timebases.videos, 99, 101)
 
+
+def test_trials():
+    paths = load_paths()
+
+    # metadata
+    metadata = npack.metadata.metadata_from_rawdata(
+        paths.session,
+        paths.source.rawdata,
+    )
+
+    # timebase
+    triggers, timebases = npack.packaging.core.load_timebases(
+        metadata=metadata, rawfile=paths.source.rawdata, verbose=False
+    )
+
     # trials
-    trials = npack.packaging.trials.load_downsampled_trials(
+    trials = npack.packaging.trials.load_trials(
         rawfile=paths.source.rawdata,
     )
     assert isinstance(trials, pd.DataFrame)
     assert trials.shape[1] < 10
     assert trials.shape[0] > 50
 
+    # downsampled trials
+    trials_ds = npack.packaging.trials.load_downsampled_trials(
+        rawfile=paths.source.rawdata,
+    )
+    assert isinstance(trials_ds, pd.DataFrame)
+
+    # compare raw vs downsampled
+    assert trials_ds.shape == trials.shape
+    for column in (
+        'reaction_time',
+        'trial_outcome',
+        'pull_duration_for_success'
+    ):
+        assert np.all(nancompare(
+            trials[column].values, trials_ds[column].values
+        ))
+    for column in (
+        'pull_onset',
+        'trial_start',
+        'trial_end',
+    ):
+        assert np.all(nancompare(
+            trials[column].values, trials_ds[column].values, tol=0.05,
+        ))
+
 
 def assert_pulse_freq(timebase, fmin, fmax):
     assert timebase.size > 2
     fs = 1 / np.diff(timebase).mean()
     assert (fs >= fmin) and (fs <= fmax)
+
+
+def nancompare(vec1, vec2, tol=1e-6):
+    valid1 = ~np.isnan(vec1)
+    valid2 = ~np.isnan(vec2)
+    valid = np.array(valid1 == valid2)
+    valid_both = np.logical_and(valid1, valid2)
+    valid[valid_both] = np.abs(vec1[valid_both] - vec2[valid_both]) <= tol
+    return valid
+
+
+def test_rawdata_nwb():
+    """tests writing the DAQ-log entries and trials to the NWBFile object"""
+    if TEMP_NWBROOT.exists():
+        shutil.rmtree(TEMP_NWBROOT)
+    paths = load_paths()
+    nwbfile = npack.packaging.package_nwb(
+        paths,
+        copy_videos=False,
+        register_rois=False,
+        write_imaging_frames=False
+    )
+    assert (nwbfile is not None)
